71e4ae9b746ea46dd3a7892facebfd0045746226
Syed Jawad Akhtar <syedjawadakhtar@gmail.com>
2019-09-14 23:39:17 +0530

    Changes wrt get the laser values

--- src/husky_high_level_controller/src/scan.py
+++ src/husky_high_level_controller/src/scan.py
@@ -4,13 +4,13 @@ import rospy
 from sensor_msgs.msg import LaserScan
 
 def callback(msg):
-    print len(msg.ranges)
+    rospy.loginfo (msg.ranges)
 
 def scan():
-	rospy.init_node('scan')
-	topicname = rospy.get_param('~topic')
-	queuesize = rospy.get_param('~queue')
-	sub = rospy.Subscriber(name=topicname, data_class=LaserScan, callback=callback, queue_size= queuesize)
+	rospy.init_node('las_scan', anonymous=True)
+	# topicname = rospy.get_param('~topic')
+	# queuesize = rospy.get_param( '~queue')
+	sub = rospy.Subscriber(name=sensor_msgs, data_class=LaserScan, callback=callback, queue_size= queuesize)
 	rospy.spin()
 	
 
